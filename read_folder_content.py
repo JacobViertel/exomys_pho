e901734fb77ae765c7cc4ce9d5745187417cbb9a
JacobViertel <jacobviertel@gmx.de>
2021-02-25 15:13:52 +0100

    visualisation of measured datapoints

---
+++ read_folder_content.py
@@ -0,0 +1,52 @@
+import os
+import csv
+import pandas
+import matplotlib.pyplot as plt
+
+entries = os.listdir('data/')
+
+colum_names =[]
+zugkraft_ar = []
+drehmoment_ar = []
+time_ar = []
+datapoint_ar = []
+drehwinkel_ar = []
+
+with open('data/'+entries[0]) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            for i in row:
+                colum_names.append(i)
+                line_count +=1
+
+print(colum_names)
+
+with open('data/'+entries[0], newline='') as csvfile:
+    data = csv.DictReader(csvfile)
+    for row in data:
+        zugkraft_ar.append(float(row[colum_names[2]]))
+        drehmoment_ar.append(float(row[colum_names[3]]))
+        drehwinkel_ar.append(float(row[colum_names[4]]))
+        time_ar.append(i)
+
+increment = 0
+for i in zugkraft_ar:
+    datapoint_ar.append(increment)
+    increment = increment +1
+
+plt.plot(datapoint_ar, zugkraft_ar, label = "Zugkraft")
+plt.plot(datapoint_ar, drehmoment_ar, label = "Drehmoment")
+plt.plot(datapoint_ar, drehwinkel_ar, label = "Drehwinkel")
+
+plt.title('Kraftverlauf Teststand')
+plt.xlabel('Datenpunkt')
+plt.ylabel('Zugkraft/Drehmoment')
+
+plt.legend()
+plt.show()
+
+
+
+
