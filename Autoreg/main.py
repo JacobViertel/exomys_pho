6cafd75ad990977bd42018397a71a05c30afcb50
JacobViertel <jacobviertel@gmx.de>
2021-03-05 13:51:31 +0100

    autoreg

--- Autoreg/main.py
+++ Autoreg/main.py
@@ -1,14 +1,29 @@
 # create and evaluate an updated autoregressive model
 from pandas import read_csv
 from matplotlib import pyplot
+import matplotlib.pyplot as plt
 from statsmodels.tsa.ar_model import AutoReg
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 # load dataset
-series = read_csv('../data/prepared_results.csv', header=0, index_col=0, parse_dates=True, squeeze=True)
+df = read_csv('../data/random_data.csv', header=0, index_col=0, parse_dates=True, squeeze=True)
+
+df = df[['zugkraft', 'drehwinkel']]
+tot_work = []
+sum_work = 0
+for x in df['zugkraft']:
+    sum_work += x*0.5
+    tot_work.append(sum_work)
+
+df['arbeit'] = (tot_work)
+df.drop(['zugkraft', 'drehwinkel'], inplace=True, axis=1)
+
 # split dataset
-X = series.values
+X = df.values
+train, test = X[1:len(X)-20], X[len(X)-20:]
+# split dataset
+X = df.values
 train, test = X[1:len(X)-20], X[len(X)-20:]
 # train autoregression
 window = 40
@@ -32,6 +47,9 @@ for t in range(len(test)):
 rmse = sqrt(mean_squared_error(test, predictions))
 print('Test RMSE: %.3f' % rmse)
 # plot
-pyplot.plot(test, color='green')
-pyplot.plot(predictions, color='red')
+pyplot.plot(test, color='green', label='Test')
+pyplot.plot(predictions, color='red', label='Vorhersage')
+plt.xlabel('Zeit')
+plt.ylabel('Arbeitslast')
+plt.legend()
 pyplot.show()
